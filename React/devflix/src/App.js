5d3b567370a3a7acd95ed8d86a6176e0275c5bd6
emanuelleao <eemanuelleao@gmail.com>
2020-07-28 00:30:35 -0300

    Started a new react project: DevFlix

---
+++ React/devflix/src/App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import Menu from './components/menu';
+function App() {
+  return (
+    <div>
+      <Menu />
+      isso é o jsx
+      Testando o React!
+      <h1>Hello React</h1>
+    </div>  
+  );
+}
+
+export default App;
